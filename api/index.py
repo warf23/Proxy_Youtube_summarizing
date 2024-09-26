6b012435f6101718f7e7c1bb4d72de17b598e145
Mohammed Agrat <54336050+warf23@users.noreply.github.com>
2024-09-26 02:17:13 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,13 +7,8 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
-from youtube_transcript_api import YouTubeTranscriptApi
-import re
 import validators
 from fastapi.middleware.cors import CORSMiddleware
-import random
-import logging
-
 
 # FastAPI app instance
 app = FastAPI()
@@ -27,64 +22,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# logging configuration
-logger = logging.getLogger(__name__)
-
 # Define a request body model
 class SummarizeRequest(BaseModel):
     groq_api_key: str
     url: str
     language: str = "English"
-
-
-# transcript script 
-
-# List of proxies
-PROXY_LIST = [
-#   "38.154.227.167:5868:ltfazxrm:y69z38mzjh2y",# not
-#   "45.127.248.127:5128:ltfazxrm:y69z38mzjh2y", #not
-#   "207.244.217.165:6712:ltfazxrm:y69z38mzjh2y", #not
-  "64.64.118.149:6732:ltfazxrm:y69z38mzjh2y", #working 
-  "167.160.180.203:6754:ltfazxrm:y69z38mzjh2y", #working 
-#   "104.239.105.125:6655:ltfazxrm:y69z38mzjh2y", # not 
-#   "198.105.101.92:5721:ltfazxrm:y69z38mzjh2y", #not
-  "154.36.110.199:6853:ltfazxrm:y69z38mzjh2y",#working 
-#   "204.44.69.89:6342:ltfazxrm:y69z38mzjh2y",# not
-#   "206.41.172.74:6634:ltfazxrm:y69z38mzjh2y"#not
-]
-
-def get_random_proxy():
-  proxy = random.choice(PROXY_LIST)
-  host, port, user, password = proxy.split(':')
-  print(f"Using proxy: {host}:{port}")
-  return {
-      "http": f"socks5://{user}:{password}@{host}:{port}",
-      "https": f"socks5://{user}:{password}@{host}:{port}"
-  }
-
-
-
-def get_youtube_transcript(video_id, language='en'):
-  try:
-      proxies = get_random_proxy()
-      transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=[language], proxies=proxies)
-      return " ".join([entry['text'] for entry in transcript])
-  except Exception as e:
-      logger.error(f"Error fetching YouTube transcript: {str(e)}")
-      return None
-
-def get_youtube_video_id(url):
-  video_id_match = re.search(r"(?:v=|\/)([0-9A-Za-z_-]{11}).*", url)
-  if video_id_match:
-      return video_id_match.group(1)
-  return None
-
-
-
-
-
-
 
 # Define the prompt template
 prompt_template = """
@@ -150,9 +92,7 @@ async def summarize(request: SummarizeRequest):
 
         # Load the URL content
         if "youtube.com" in url:
-            video_id = get_youtube_video_id(url)
-            if video_id:
-              combined_text = get_youtube_transcript(video_id, language_codes.get(language, 'en'))
+            loader = YoutubeLoader.from_youtube_url(url, language=language_codes[language], add_video_info=True)
         else:
             loader = UnstructuredURLLoader(
                 urls=[url], ssl_verify=False, headers={"User-Agent": "Mozilla/5.0"}
@@ -160,7 +100,6 @@ async def summarize(request: SummarizeRequest):
 
         docs = loader.load()
 
-        
         # Combine the documents
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.split_documents(docs)
@@ -237,7 +176,7 @@ Formatting:
 - Use clear, concise language appropriate for the {expertise_level} in {focus_area}.
 - Employ bullet points or numbered lists for clarity when presenting multiple related points.
 - Include subheadings if it enhances readability and organization.
-
+- add mutiple color to enhance the visualization 
 
 Note: Ensure the summary stands alone as an informative piece, providing value even without access to the original content, while being specifically tailored to {focus_area} at an {expertise_level} level.
 """
@@ -307,7 +246,7 @@ async def summarize_advanced(request: AdvancedSummarizeRequest):
         raise HTTPException(status_code=500, detail=f"Error occurred: {str(e)}")
 
 
-# Run with Uvicorn
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+# # Run with Uvicorn
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
